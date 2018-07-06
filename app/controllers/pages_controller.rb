211ca7e7660032f687212496c580327c61161789
htharker42 <htharker@gmail.com>
2018-07-06 14:13:49 -0700

    added basic routes and associated files

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,28 @@
 class PagesController < ApplicationController
+  def home
+  end
+
+  def contact
+  end
+
+  def about
+  end
+
+  # def projects
+  #   #route projects to here
+  #   #layout should be different as well
+  # end
+
+  def news
+    #@tweets = SocialTool.twitter_search
+    #route model to here add
+    #@news = News.recent.page(params[:page]).per(10)
+  end
+
+  # private
+
+  # def news_params
+    # params.require(:news).permit(:title, :body)
+  # end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+root to: 'pages#home'
+
+get '/about', to: 'pages#about'
+
+get '/contact', to: 'pages#contact'
+
+get '/news', to: 'pages#news'
+
+#get '/projects', to: 'projects#show', as: 'projects_show'
+
 end
