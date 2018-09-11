1a395b019f84068232216f1d0472e6b38fcd35bf
htharker42 <htharker@gmail.com>
2018-09-11 14:52:47 -0700

    added volunteer model, controller, and routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  resources :volunteers, except: [:index, :show, :edit]
   root to: 'pages#home'
 
   get '/contact', to: 'pages#contact'
