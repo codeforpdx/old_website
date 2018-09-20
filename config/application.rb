35708b01c2ecae77ea71ff5d7c1b34f35b98afec
htharker42 <htharker@gmail.com>
2018-09-20 15:09:39 -0700

    fixed syntax error

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,6 @@ module Codeforpdx
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+  config.secret_key_base = ENV["SECRET_KEY_BASE"]
   end
 end
